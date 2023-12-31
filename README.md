17f3bb29516fa858b5aa5a860564d79cd5c9edb6
ruuuu <rufinka_91@mail.ru>
2023-12-31 11:55:51 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Документация: https://v3.ru.vuejs.org/ru/guide/installation.html#npm https://vuejs.org/guide/quick-start.html
+
 # deliveryVueJS
 
 This template should help get you started developing with Vue 3 in Vite.
---
+++ src/constants/restaurants.js
@@ -0,0 +1,57 @@
+export const restaurantsArray = [
+   {
+      id: 0,
+      title: "Пицца плюс",
+      time: 60, 
+      rating: 4.5,
+      price: 900,
+      type: 'Пицца',
+      image: 'card1.jpg'
+   }, 
+   {
+      id: 1,
+      title: "Тануки ",
+      time: 40, 
+      rating: 3.5,
+      price: 400,
+      type: 'Пицца',
+      image: 'card2.jpg'
+   },
+   {
+      id: 2,
+      title: "FoodBand",
+      time: 50, 
+      rating: 2.0,
+      price: 700,
+      type: 'Пицца',
+      image: 'card3.jpg'
+   },
+   {
+      id: 3,
+      title: "Жадина-пицца",
+      time: 50, 
+      rating: 2.0,
+      price: 700,
+      type: 'Пицца',
+      image: 'card4.jpg'
+   },
+   {
+      id: 4,
+      title: "Точка еды",
+      time: 30, 
+      rating: 1.0,
+      price: 1000,
+      type: 'Пицца',
+      image: 'card5.jpg'
+   },
+   {
+      id: 5,
+      title: "PizzaBurger",
+      time: 50, 
+      rating: 6.0,
+      price: 800,
+      type: 'Пицца',
+      image: 'card6.jpg'
+   }
+
+];
